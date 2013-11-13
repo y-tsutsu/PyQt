11fed6f2f78920ab35613f91ce7cb166f1526a75
y-tsutsumi <tsutsu0928@gmail.com>
2013-11-13 22:35:04 +0900

    setup.pyでlibEGL.dllが漏れるのを修正．

--- PyQt/setup.py
+++ PyQt/setup.py
@@ -1,14 +1,15 @@
 import sys
 from cx_Freeze import setup, Executable
  
-includes = ["PyQt5.QtCore","PyQt5.QtGui","PyQt5.QtWidgets","re",]
- 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
  
 setup(  name = "ramen_timer",
         version = "1.0",
-        options = {"build_exe": {"includes": includes}},
+        options = {"build_exe": {
+			"includes": "atexit",
+			"include_files": [(r"c:\python33\lib\site-packages\PyQt5\libEGL.dll", "")]
+		}},
         executables = [Executable("ramen_timer.pyw", base=base)])
 		
