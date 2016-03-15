21590901d009645eacd98a670ea170c51638978f
tsutsu <tsutsu0928@gmail.com>
2016-03-15 23:34:24 +0900

    設定ファイルを更新．

--- PyQt/setup.py
+++ PyQt/setup.py
@@ -1,15 +1,14 @@
 import sys
 from cx_Freeze import setup, Executable
- 
+
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
- 
+
 setup(  name = "ramen_timer",
         version = "1.0",
         options = {"build_exe": {
-			"includes": "atexit",
-			"include_files": [(r"c:\python34\lib\site-packages\PyQt5\libEGL.dll", "")]
-		}},
+            "includes": "atexit",
+            "include_files": [(r"c:\python34\lib\site-packages\PyQt5\libEGL.dll", "")]
+        }},
         executables = [Executable("ramen_timer.pyw", base=base)])
-		
